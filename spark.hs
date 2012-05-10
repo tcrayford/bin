0dc527bad4df11ccdbd3c567a8734e1db8c05d8c
Tom Crayford <tcrayford@googlemail.com>
2012-05-10 19:52:36 +0100

    add spark

---
+++ spark.hs
@@ -0,0 +1,13 @@
+-- Generate a sparkline, Haskell version of <https://github.com/holman/spark/>
+-- -- Usage: after compile, try '$ spark -10 0 15.24 42'
+-- -- Author: Chao Xu, <http://chaoxuprime.com>
+import System (getArgs)
+
+main :: IO ()
+main = do input <- getArgs
+          putStrLn $ spark (map read input :: [Double])
+
+spark :: [Double] -> String
+spark list = map ("▁▂▃▄▅▆▇" !!) xs
+            where zs = map (flip (-) (minimum list)) list
+                  xs = map (round . (* 6) . (/ maximum zs)) zs
